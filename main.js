580e8f578d7700f1eb8ed03aa448dc0fbc149ea1
Bohdan web2025-5 <bohdan.hrynivv@gmail.com>
2025-04-20 11:41:25 +0300

    add part1

--- main.js
+++ main.js
@@ -1,2 +1,21 @@
 import { program } from 'commander';
 import superagent from 'superagent';
+import http from 'http';
+
+program
+    .requiredOption('-h, --host <host>', 'Server address')
+    .requiredOption('-p, --port <port>', 'Server port')
+    .requiredOption('-c, --cache <path>', 'Path to cache directory')
+    .parse(process.argv);
+
+const options = program.opts();
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Server is running\n');
+});
+
+server.listen(options.port, options.host, () => {
+});
+
+
